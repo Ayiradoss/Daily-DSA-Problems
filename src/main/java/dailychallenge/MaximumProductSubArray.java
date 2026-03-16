848c16f431178ffe4ec4b597764963206924163a
Ayira Doss <ayiradoss20@example.com>
2026-03-16 20:49:22 +0530

    Day 26 Problem

---
+++ src/main/java/dailychallenge/MaximumProductSubArray.java
@@ -0,0 +1,40 @@
+package dailychallenge;
+
+public class MaximumProductSubArray {
+    public static void main(String[] args) {
+        int[] nums = {2,3,-2,4};
+
+        System.out.print(maxProduct(nums));
+    }
+    public static int maxProduct(int[] nums)
+    {
+        int maxending = nums[0];
+
+        int minending = nums[0];
+
+        int maxsofar = nums[0];
+
+        if(nums.length == 1)
+        {
+            return nums[0];
+        }
+
+        for(int i = 1; i < nums.length; i++)
+        {
+            if(nums[i] < 0)
+            {
+                int temp = maxending;
+
+                maxending = minending;
+
+                minending = temp;
+            }
+            maxending = Math.max(nums[i], maxending * nums[i]);
+
+            minending = Math.min(nums[i], minending * nums[i]);
+
+            maxsofar = Math.max(maxending, maxsofar);
+        }
+        return maxsofar;
+    }
+}
